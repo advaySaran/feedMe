9ec58c596e57ba577ce3c8f5f6a2af5ed40aefcf
advaySaran <74816441+advaySaran@users.noreply.github.com>
2021-08-30 21:05:29 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,11 +1,8 @@
 var dog,sadDog,happyDog, database;
 var foodS,foodStock;
-var addFood;
+var fedTime,lastFed;
+var feed,addFood;
 var foodObj;
-
-//create feed and lastFed variable here
-var feed;
-var lastFed;
 
 function preload(){
 sadDog=loadImage("Dog.png");
@@ -13,20 +10,20 @@ happyDog=loadImage("happy dog.png");
 }
 
 function setup() {
-  database=firebase.database();
+database=firebase.database();
   createCanvas(1000,400);
 
   foodObj = new Food();
 
-  foodStock=database.ref('Food');
-  foodStock.on("value",readStock);
+ foodStock=database.ref('Food');
+foodStock.on("value",readStock);
+
   
   dog=createSprite(800,200,150,150);
   dog.addImage(sadDog);
   dog.scale=0.15;
-
-  //create feed the dog button here
-  feed=createButton("feed the dog");
+  
+  feed=createButton("Feed the dog");
   feed.position(700,95);
   feed.mousePressed(feedDog);
 
@@ -38,15 +35,17 @@ function setup() {
 
 function draw() {
   background(46,139,87);
-  foodObj.display(); 
+  foodObj.display();
 
-if(lastFed>=12){
-}else if (lastFed==0){
-text("Last feed : 12 AM", 350,30);
-}else{
-
-}
-
+  fedTime=database.ref('FeedTime');
+ fedTime.on("value",function(data){ lastFed=data.val(); });
+ fill(255,255,254); 
+textSize(15);
+ if(lastFed>=12){
+ text("Last Feed : "+ lastFed%12 + " PM", 350,30); }
+else if(lastFed==0){
+ text("Last Feed : 12 AM",350,30); }
+else{ text("Last Feed : "+ lastFed + " AM", 350,30); }
  
   drawSprites();
 }
@@ -58,13 +57,21 @@ function readStock(data){
 }
 
 
+//function to update food stock and last fed time
 function feedDog(){
   dog.addImage(happyDog);
-  foodS--;
+  
+  var food_stock_val = foodObj.getFoodStock();
+  if(food_stock_val <= 0){
+      foodObj.updateFoodStock(food_stock_val *0);
+  }else{
+      foodObj.updateFoodStock(food_stock_val -1);
+  }
+  
   database.ref('/').update({
-    Food:foodS
+    Food:foodObj.getFoodStock(),
+    FeedTime:hour()
   })
-
 }
 
 //function to add food in stock
@@ -74,3 +81,4 @@ function addFoods(){
     Food:foodS
   })
 }
+
